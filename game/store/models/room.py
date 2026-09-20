5b716075e0905916cf13a871e8e92fb8307f0d49
Daniel Bingham <dbingham@theroadgoeson.com>
2026-09-20 11:53:54 -0400

    wip(feat): typing and validation for Rooms and Exits

--- game/store/models/room.py
+++ game/store/models/room.py
@@ -1,7 +1,43 @@
 from __future__ import annotations
 
+from typing import TYPE_CHECKING
+
+from enum import StrEnum
+
 from game.store.models.base import JsonSerializable
 from game.store.models.base import Model
+
+if TYPE_CHECKING:
+    from game.store.models.character import Character
+    from game.store.models.item import Item
+
+class Direction(StrEnum):
+    "The directions you may travel from one room to the next."
+
+    NORTH = "north"
+    EAST = "east"
+    SOUTH = "south"
+    WEST = "west"
+    UP = "up"
+    DOWN = "down"
+
+class WaterType(StrEnum):
+    "The types of water that exist in the world."
+
+    NONE = "none"
+    SALT = "salt"
+    FRESH = "fresh"
+
+
+# A helper to invert a direction.
+INVERT_DIRECTION = {
+    Direction.NORTH: Direction.SOUTH,
+    Direction.EAST: Direction.WEST,
+    Direction.SOUTH: Direction.NORTH,
+    Direction.WEST: Direction.EAST,
+    Direction.UP: Direction.DOWN,
+    Direction.DOWN: Direction.UP
+}
 
 class Exit(JsonSerializable):
     "An exit from one room to another."
@@ -16,6 +52,11 @@ class Exit(JsonSerializable):
     # rooms have been loaded, so this isn't deserialized by fromJson().
     room_to: Room | None
 
+    # An object link to the other side of this exit in `room_to`. Set by store
+    # after all rooms have been loaded, so this isn't deserialized by
+    # `fromJson()`.
+    exit_to: Exit | None
+
     # Is this exit a door that can be opened and closed? If yes, True.  False
     # otherwise. Optional.
     is_door: bool
@@ -24,15 +65,18 @@ class Exit(JsonSerializable):
     # closed. Optional.
     is_open: bool
 
+    # The direction this exit goes. Required.
+    direction: Direction
+
     def __init__(self, room):
         self.room_from = room
+        self.room_to = None
+        self.exit_to = None
 
         self.is_door = False
         self.is_open = True
 
-        self.direction = ''
-        self.room_to = None
-        self.exit_to = None
+        self.direction = Direction.NORTH
 
     def toJson(self):
         json = {}
@@ -78,6 +122,40 @@ class Room(Model):
     WATER_SALT = 'salt'
     WATER_FRESH = 'fresh'
 
+    # The room's title, displayed at the top of the room description. A short
+    # descriptive phrase. eg. "An Ancient Forest". Required.
+    title: str
+
+    # The room's long description. A paragraph describing the area contained in
+    # the room. Required.
+    description: str
+
+    # A color definition in RGB format. A list of three integers representing
+    # the RGB color code. Required.
+    color: list[int]
+
+    # What type of water is present in this room (if any). Optional.
+    water_type: WaterType
+
+    # How deep the water in this room is. (meters) Optional.
+    water: float
+
+    # How fast the water in this room is moving. (meters per second) Optional.
+    water_velocity: float
+
+    # A dictionary of exits that lead out of this room and connect to other
+    # rooms. Optional.
+    exits: dict[Direction, Exit]
+
+    # A list of Characters who are currently present in this room. Serialized
+    # as a list of Ids and converted to object references by Store after
+    # loading. Optional.
+    occupants: list[Character]
+
+    # A list of Items currently present in this room. Serialized as a list of
+    # Ids and converted to object references by Store after loading. Optional.
+    items: list[Item]
+
     def __init__(self):
         super(Room, self).__init__()
 
@@ -85,11 +163,8 @@ class Room(Model):
         self.description = ''
         self.color = []
 
-        # What type of water is present in this room (if any).
-        self.water_type = self.WATER_NONE
-        # How deep the water in this room is. (meters)
+        self.water_type = WaterType.NONE
         self.water = 0
-        # How fast the water in this room is moving. (meters per second)
         self.water_velocity = 0
 
         self.exits = {}
@@ -104,7 +179,7 @@ class Room(Model):
         json['description'] = self.description
         json['color'] = self.color
 
-        if self.water_type != self.WATER_NONE:
+        if self.water_type != WaterType.NONE:
             json['waterType'] = self.water_type
             json['water'] = self.water
             json['waterVelocity'] = self.water_velocity
@@ -119,6 +194,11 @@ class Room(Model):
 
         json['occupants'] = []
         for occupant in self.occupants:
+
+            # We don't store player characters in the room serializations.
+            # Instead, the room the character is in is stored in the character
+            # itself.  They will be loaded into that room when they log back
+            # in.
             if occupant.is_player_character:
                 continue
             json['occupants'].append(occupant.getId())
@@ -144,6 +224,8 @@ class Room(Model):
         # Store::load
         self.items = data['items']
 
+        # Store will convert the list of ids into object references in
+        # Store::load
         if 'occupants' in data:
             self.occupants = data['occupants']
 
